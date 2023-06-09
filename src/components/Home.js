f6bf35b9b706e45af9cc2c1216634f49967a3ee9
vansh sharma <sharmaprashant872@gmail.com>
2023-06-09 13:14:00 +0530

    Added Blogcard Component (Refactor).

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,13 @@
-import React, { useContext } from 'react'
-import blogContext from '../context/blogContext'
+import React from 'react'
+
+import Blogcard from './Blogcard'
 
 export default function Home() {
-  const { blogs } = useContext(blogContext)
+
   return (
     <>
       <div className="container">
-
-        <div className="my-3 row">
-          {blogs && blogs.map((blog) => {
-            return <div className="card mx-1" key={blog._id} style={{ width: "18rem" }}>
-              {/* <img src="..." className="card-img-top" alt="..."/> */}
-              <div className="card-body">
-                <h5 className="card-title">{blog.title}</h5>
-                <p className="card-text">{blog.description}</p>
-                <a href="/" className="btn btn-primary">Read More...</a>
-              </div>
-            </div>
-          })}
-        </div>
-
+        <Blogcard />
       </div>
     </>
   )
