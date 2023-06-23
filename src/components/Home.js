29e366edb3ec4cee6e7acb9ea8d211c226080ce0
vansh sharma <sharmaprashant872@gmail.com>
2023-06-23 19:21:01 +0530

    Added Shortcut...

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,28 @@
 import React, { useContext, useEffect } from 'react'
+import  { useNavigate } from 'react-router-dom'
 import BlogContext from '../context/blogContext'
 import AuthContext from '../context/authContext'
 import Blogcard from './Blogcard'
 import Alert from './Alert'
 
 export default function Home(props) {
+  const navigate=useNavigate()
   const { setGenre } = useContext(BlogContext)
   const { alert } = useContext(AuthContext)
   useEffect(() => {
     setGenre(props.category)
     // eslint-disable-next-line
   }, [])
+  document.onkeydown=(e)=>{
+    console.log(e)
+    if(e.ctrlKey && e.shiftKey && (e.key==="a"||e.key==="A")){
+      navigate('/anime')
+    }else if(e.ctrlKey && e.shiftKey && (e.key==="g"||e.key==="G")){
+      navigate('/general')
+    }else if(e.ctrlKey && e.shiftKey && (e.key==="p"||e.key==="P")){
+      navigate('/programming')
+    }
+  }
 
   return (
     <>
